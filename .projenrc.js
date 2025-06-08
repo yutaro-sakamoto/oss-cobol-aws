15a797beca8ad00f35b56a12a5627c297bfb7ff7
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-06-08 23:02:39 +0900

    add: cdk project

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { javascript } = require("projen");
+const { javascript, awscdk } = require("projen");
 const project = new javascript.NodeProject({
   defaultReleaseBranch: "main",
   name: "oss-cobol-aws",
@@ -8,4 +8,18 @@ const project = new javascript.NodeProject({
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+// Add CDK subproject
+new awscdk.AwsCdkTypeScriptApp({
+  parent: project,
+  outdir: "cdk",
+  name: "oss-cobol-aws-cdk",
+  defaultReleaseBranch: "main",
+  cdkVersion: "2.125.0",
+  description: "CDK infrastructure for OSS COBOL AWS project",
+  packageManager: javascript.NodePackageManager.YARN,
+  prettier: true,
+  projenrcTs: true,
+});
+
 project.synth();
